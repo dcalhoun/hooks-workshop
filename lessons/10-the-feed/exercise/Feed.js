1b299129522dd2fecef858b9e6f5da9b6eb89e0f
David Calhoun <dpcalhoun@gmail.com>
2019-05-22 15:31:56 -0600

    [add] Feed component

--- lessons/10-the-feed/exercise/Feed.js
+++ lessons/10-the-feed/exercise/Feed.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import FeedPost from "app/FeedPost"
 import { loadFeedPosts, subscribeToNewFeedPosts } from "app/utils"
 // import FeedFinal from './Feed.final'
@@ -6,29 +6,56 @@ import { loadFeedPosts, subscribeToNewFeedPosts } from "app/utils"
 export default Feed
 
 function Feed() {
+  const [createdAt, setCreatedAt] = useState(Date.now())
+  const [posts, setPosts] = useState([])
+  const [newPosts, setNewPosts] = useState([])
+  const [limit, setLimit] = useState(3)
+  useEffect(() => {
+    let isCurrent = true
+    loadFeedPosts(createdAt, limit).then(fetchedPosts => {
+      if (isCurrent) setPosts(fetchedPosts)
+    })
+    return () => (isCurrent = false)
+  }, [createdAt, limit])
+
+  useEffect(() => {
+    return subscribeToNewFeedPosts(createdAt, incomingPosts => {
+      setNewPosts(incomingPosts)
+    })
+  }, [createdAt])
+
   return (
     <div className="Feed">
+      {newPosts.length > 0 && (
+        <div className="Feed_button_wrapper">
+          <button
+            className="Feed_new_posts_button icon_button"
+            onClick={() => {
+              setPosts([...newPosts, ...posts])
+              setLimit(limit + newPosts.length)
+              setCreatedAt(Date.now())
+              setNewPosts([])
+            }}
+          >
+            View {newPosts.length} New Posts
+          </button>
+        </div>
+      )}
+
+      {posts.map(post => (
+        <FeedPost key={post.id} post={post} />
+      ))}
+
       <div className="Feed_button_wrapper">
-        <button className="Feed_new_posts_button icon_button">
-          View 3 New Posts
+        <button
+          className="Feed_new_posts_button icon_button"
+          onClick={() => {
+            setLimit(limit + 3)
+          }}
+        >
+          View More
         </button>
-      </div>
-
-      <FeedPost post={fakePost} />
-
-      <div className="Feed_button_wrapper">
-        <button className="Feed_new_posts_button icon_button">View More</button>
       </div>
     </div>
   )
 }
-
-// you can delete this
-const fakePost = {
-  createdAt: Date.now() - 10000,
-  date: "2019-03-30",
-  message: "Went for a run",
-  minutes: 45,
-  uid: "0BrC0fB6r2Rb5MNxyQxu5EnYacf2"
-}
-
