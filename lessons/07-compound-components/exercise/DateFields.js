e3a1153014d82fb0366a8a5145d7f990c3d5eb6f
David Calhoun <dpcalhoun@gmail.com>
2019-05-22 10:23:06 -0600

    [add] Compound Component with shared context

--- lessons/07-compound-components/exercise/DateFields.js
+++ lessons/07-compound-components/exercise/DateFields.js
@@ -1,6 +1,8 @@
-import React, { Fragment, useContext, createContext } from "react"
+import React, { useContext, createContext } from "react"
 
 import { daysInMonth } from "app/utils"
+
+const DatesFieldContext = createContext()
 
 export default function DateFields({
   children,
@@ -12,16 +14,14 @@ export default function DateFields({
 }) {
   const date = controlledValue || defaultValue
   return (
-    <Fragment>
-      <MonthField date={date} onChange={onChange} />/
-      <DayField date={date} onChange={onChange} />/
-      <YearField date={date} onChange={onChange} start={start} end={end} />
-    </Fragment>
+    <DatesFieldContext.Provider value={{ date, onChange }}>
+      {children}
+    </DatesFieldContext.Provider>
   )
 }
 
 export function DayField(props) {
-  const { date, onChange } = props
+  const { date, onChange } = useContext(DatesFieldContext)
   const month = date.getMonth()
   const year = date.getFullYear()
   const days = Array.from({ length: daysInMonth(month, year) })
@@ -45,7 +45,7 @@ export function DayField(props) {
 }
 
 export function MonthField(props) {
-  const { date, onChange } = props
+  const { date, onChange } = useContext(DatesFieldContext)
   const month = date.getMonth()
   const handleChange = event => {
     const newDate = new Date(date.getTime())
@@ -72,7 +72,8 @@ export function MonthField(props) {
 }
 
 export function YearField(props) {
-  const { date, onChange, start, end } = props
+  const { start, end } = props
+  const { date, onChange } = useContext(DatesFieldContext)
   const difference = end - start + 1
   const years = Array.from({ length: difference }).map(
     (_, index) => index + start
